1eb88ef3bb345b9fbd984ad683556e8de503c423
NSDawn <64914030+NSDawn@users.noreply.github.com>
2020-11-24 00:30:48 -0800

    Add files via upload

--- petals.js
+++ petals.js
@@ -64,6 +64,7 @@ function init() {
 
   // inititalization of value (helps with scaling idek)
   const cs = new Vector2(600, 600, 'rect'); // cs = canvas_size
+  const CTR = new Vector2(cs.x/2, cs.y/2, 'rect');
 
   // inititalization of svg object 
   const svg = d3.select("body").append("svg")
@@ -77,38 +78,66 @@ function init() {
 
   // csv parsing
   d3.csv("data_clean.csv").then(function(data) {
+    data_length = data.length;
     for (let i = 0; i < data.length; i++) {
-      const petal_data = new Petal(data[i].Activity, data[i].Mood, data[i].Significance, data[i].Notes, '#cf5af8');
+      const petal_data = new Petal(data[i].Activity, data[i].Mood, data[i].Significance, data[i].Notes, significanceColor(data[i].Significance));
       petals.push(petal_data);
     }
+    // assigning path shapes to elements in petals[]
+    for (let i = 0; i < petals.length; i++) {
+      const ENDPT = new Vector2(200, (((2/PHI) * i) % 2) * PI, 'polar'); // change the first value of Vector2 for size.
+      const UNDERMIDPT1 = new Vector2(ENDPT.r / 3, ENDPT.theta - PI/6, 'polar'); 
+      const UNDERMIDPT2 = new Vector2(ENDPT.r * 2/3, ENDPT.theta - PI/6, 'polar'); 
+      const OVERMIDPT1 = new Vector2(ENDPT.r / 3, ENDPT.theta + PI/6, 'polar');
+      const OVERMIDPT2 = new Vector2(ENDPT.r * 2/3, ENDPT.theta + PI/6, 'polar');
+      petals[i].path = svg.append('path')
+        .attr("d", p("M", CTR.x, CTR.y) + s + p("C", UNDERMIDPT1.cx, UNDERMIDPT1.cy, UNDERMIDPT2.cx, UNDERMIDPT2.cy, ENDPT.cx, ENDPT.cy) + s +  p("C", OVERMIDPT2.cx, OVERMIDPT2.cy, OVERMIDPT1.cx, OVERMIDPT1.cy, CTR.x, CTR.y))
+        .attr("stroke", petals[i].color)
+        .attr("stroke_width", "2")
+        .attr("fill", petals[i].color)
+      ;
+    }
   }); 
-  
 
-  console.log(petals);
-  
+  function significanceColor(in_int) {
+    let output;
+    switch (in_int) {
+      case "0":
+        output = "#8cd1ff";
+        break;
+      case "1":
+        output = "#82eaff";
+        break;
+      case "2":
+        output = "#73fffa";
+        break;
+      case "3":
+        output = "#65fcdc";
+        break;
+      case "4": 
+        output = "#59ffbf";
+        break;
+      case "5": 
+        output = "#48fab5";
+        break;  
+      case "6":
+        output = "#3bf58b";
+      default:
+        console.log("significance value could not be read.");
+        output = false;
+    }
+    return output;
+  }
   // garbage data lmao
+  /*
   const garbagedata1 = new Petal("Snuggled a cone of human skin", 6, 4, "It was cute lmao.", "#ff641c");
   petals.push(garbagedata1);
   const garbagedata2 = new Petal("Licked my table", 1, 2, "Tasted like dust.", "#325478");
   petals.push(garbagedata2);
   const garbagedata3 = new Petal("死にたかった。", 0, 4, "誰もいない。彼女をください。", "#cf5fa8");
   petals.push(garbagedata3);
-  const garbagedata4 = new Petal("eoawgeoiaeogaw", 2, 4, "fowamefemwpgegpegw", "cf5af8");
+  const garbagedata4 = new Petal("eoawgeoiaeogaw", 2, 4, "fowamefemwpgegpegw", "#cf5af8");
   petals.push(garbagedata4); 
-  
-  // assigning path shapes to elements in petals[]
-  const CTR = new Vector2(cs.x/2, cs.y/2, 'rect');
-  for (let i = 0; i < petals.length; i++) {
-    const ENDPT = new Vector2(200, (((2/PHI) * i) % 2) * PI, 'polar'); // change the first value of Vector2 for size.
-    const UNDERMIDPT1 = new Vector2(ENDPT.r / 3, ENDPT.theta - PI/6, 'polar'); 
-    const UNDERMIDPT2 = new Vector2(ENDPT.r * 2/3, ENDPT.theta - PI/6, 'polar'); 
-    const OVERMIDPT1 = new Vector2(ENDPT.r / 3, ENDPT.theta + PI/6, 'polar');
-    const OVERMIDPT2 = new Vector2(ENDPT.r * 2/3, ENDPT.theta + PI/6, 'polar');
-    petals[i].path = svg.append('path')
-      .attr("d", p("M", CTR.x, CTR.y) + s + p("C", UNDERMIDPT1.cx, UNDERMIDPT1.cy, UNDERMIDPT2.cx, UNDERMIDPT2.cy, ENDPT.cx, ENDPT.cy) + s +  p("C", OVERMIDPT2.cx, OVERMIDPT2.cy, OVERMIDPT1.cx, OVERMIDPT1.cy, CTR.x, CTR.y))
-      .attr("stroke", petals[i].color)
-      .attr("stroke_width", "2")
-      .attr("fill", petals[i].color)
-    ;
-  }
+  */
+
 }
